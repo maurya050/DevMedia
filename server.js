8ecd5ac388d4ccb8ec32940e79ecb0149603b9eb
maurya050 <mauryashivam050@gmail.com>
2025-03-08 16:51:02 +0530

    DB Connection stablished and routes structure has been created as well

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false })); // Body parser
 
 app.get('/', (req, res) => {
     res.send("API Running");
 });
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));     // / => /api/users
+app.use('/api/auth', require('./routes/api/auth'));       // / => /api/auth
+app.use('/api/profile', require('./routes/api/profile')); // / => /api/profile
+app.use('/api/posts', require('./routes/api/posts'));     // / => /api/posts
 
 const PORT = process.env.PORT || 6000;
 
