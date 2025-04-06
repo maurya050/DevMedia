9061aab49833548ec366f1ba02ec8d2af88f52fa
maurya050 <mauryashivam050@gmail.com>
2025-04-06 20:56:02 +0530

    Route Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
-import React , {Fragment} from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
+import "./App.css";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 const App = () => (
+<Router>
   <Fragment>
-      <Navbar />
-      <Landing />
+    <Navbar />
+    <Routes>
+      <Route exact path="/" element={<Landing />} />
+      <Route exact path="/register" element={<section className="container"><Register /></section>} />
+      <Route exact path="/login" element={<section className="container"><Login /></section>} />
+    </Routes>
   </Fragment>
+</Router>
 );
 export default App;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,15 +1,32 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <a href="index.html"><i className="fas fa-code"></i> DevConnector</a>
+        <Link to="/">
+          <i className="fas fa-code" /> DevMedia
+        </Link>
       </h1>
       <ul>
-        <li><a href="profiles.html">Developers</a></li>
-        <li><a href="register.html">Register</a></li>
-        <li><a href="login.html">Login</a></li>
+        <li>
+          <Link to="/profiles">Developers</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/posts">Posts</Link>
+        </li>
+        <li>
+          <Link to="/dashboard">
+            <i className="fas fa-user" />{' '}
+            <span className="hide-sm">Dashboard</span>
+          </Link>
+        </li>
       </ul>
     </nav>
   )
